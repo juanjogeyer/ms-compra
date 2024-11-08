30d7dccbb5d87ec96f714c7e0b3fb9afc8a528a8
Juan Geyer <juanjogeyer@gmail.com>
2024-11-08 13:01:34 -0300

    Update services

--- app/mapping/compra_schema.py
+++ app/mapping/compra_schema.py
@@ -1,10 +1,10 @@
 from marshmallow import Schema, fields, post_load
-from app.model import Compra
+from app.models.compra import Compra
 
 class CompraSchema(Schema):
     id = fields.Integer(dump_only=True)
-    producto_id = fields.Integer(required=True)
-    fecha_compra = fields.DateTime(dump_only=True)
+    producto = fields.Integer(required=True)
+    fecha_compra = fields.DateTime(required=False)
     direccion_envio = fields.String(required=True)
 
     @post_load
--- app/services/compra_service.py
+++ app/services/compra_service.py
@@ -1,24 +1,16 @@
-from app.repository import CompraRepository
-from app.model import Compra
-from typing import List
+from app.compra_repository import CompraRepository
+from app.models import Compra
+from datetime import datetime
 
 repository = CompraRepository()
 
 class CompraService:
 
-    def all(self) -> List[Compra]:
-        return repository.all()
-
     def add(self, compra: Compra) -> Compra:
-        return repository.add(compra)
+        compra.fecha_compra = datetime.now()
+        result = repository.add(compra)
+        return result
 
-    def delete(self, id: int) -> bool:
-        compra = self.find(id)
-        if compra:
-            repository.delete(compra)
-            return True
-        else:
-            return False
-
-    def find(self, id: int) -> Compra:
-        return repository.find(id)
+    def delete(self, id: int) -> Compra:
+        result = repository.delete(id)
+        return result
